b0b73c9c5d6b3c42e932e9e692c2d4392d750565
Basiusia <b.banz@outlook.com>
2023-08-07 21:54:13 +0200

    just a dog update

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -14,7 +14,7 @@ interface IHomePageContent {
 @Component({
     selector: 'dog-home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.less']
+    styleUrls: ['./home.component.less'],
 })
 export class HomeComponent implements OnInit {
     public homePageContent: IHomePageContent[] = this.getPageContents();
@@ -42,7 +42,7 @@ export class HomeComponent implements OnInit {
                 text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolorin reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident',
                 btnText: 'Poznaj mnie',
                 btnType: 'primary',
-                btnFunc: () => this._router.navigate(['/aboutMe'])
+                btnFunc: () => this._router.navigate(['/o-mnie']),
             },
             {
                 photo: 'assets/images/temp/home-2.jpg',
@@ -52,7 +52,7 @@ export class HomeComponent implements OnInit {
                 text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolorin reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident',
                 btnText: 'Spotkajmy się na Facebooku',
                 btnType: 'secondary',
-                btnFunc: () => this.openFacebook()
+                btnFunc: () => this.openFacebook(),
             },
             {
                 photo: 'assets/images/temp/home-3.jpg',
@@ -62,8 +62,8 @@ export class HomeComponent implements OnInit {
                 text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolorin reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident',
                 btnText: 'Spotkajmy się na Instagramie',
                 btnType: 'primary',
-                btnFunc: () => this.openInstagram()
-            }
+                btnFunc: () => this.openInstagram(),
+            },
         ];
     }
 }
